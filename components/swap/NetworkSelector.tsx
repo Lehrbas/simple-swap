5f28b478949ddf72117aa72ccf075486f2cbb513
Lehrbas <eduardo-lehrbach@hotmail.com>
2022-05-05 02:33:37 -0300

    initialize project

---
+++ components/swap/NetworkSelector.tsx
@@ -0,0 +1,7 @@
+interface Props {
+  network: JSX.Element
+}
+
+export default function NetworkSelector(props: Props) {
+
+}
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import '../components/swap/*'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    
+  )
+}
